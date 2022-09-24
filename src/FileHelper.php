cb8bd3491d2b5eef6f62cfe76419aa9fa258538d
Lebedenko Nikolay <totx@ya.ru>
2022-09-24 13:58:50 +0300

    FileHelper : write features added

--- src/FileHelper.php
+++ src/FileHelper.php
@@ -3,15 +3,46 @@ declare(strict_types=1);
 
 namespace YaPro\Helper;
 
+use UnexpectedValueException;
+
 class FileHelper
 {
     /**
-     * @param string $fileName
+     * @param string $filePath
      * @return string|false
      */
-    public function getFileContent($fileName)
+    public function getFileContent(string $filePath)
     {
-        $file = new \SplFileObject($fileName, "r");
+        $file = new \SplFileObject($filePath, "r");
+
         return $file->fread($file->getSize());
+    }
+
+    /**
+     * @param string $filePath
+     * @param mixed $content
+     * @return string path to file
+     */
+    public function writeToFile(string $filePath, $content): string
+    {
+        if (false === file_put_contents($filePath, $content)) {
+            throw new UnexpectedValueException('Unable to save file');
+        }
+
+        return $filePath;
+    }
+
+    /**
+     * @param mixed $content
+     * @return string path to temporary file
+     */
+    public function writeToTemporaryFile($content): string
+    {
+        $filePathInTmp = tempnam(sys_get_temp_dir(), '');
+        if (false === $filePathInTmp) {
+            throw new UnexpectedValueException('Unable to create temporary file');
+        }
+
+        return $this->writeToFile($filePathInTmp, $content);
     }
 }
