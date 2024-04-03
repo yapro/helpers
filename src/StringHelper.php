821eb5b5c90f5c9488734f0bc6f9e1380d6e2781
Lebnik <totx@ya.ru>
2024-04-03 12:35:33 +0300

    new string function getHtmlWithoutIndentions

--- src/StringHelper.php
+++ src/StringHelper.php
@@ -54,9 +54,9 @@ class StringHelper
         return trim($text, '-');
     }
 
-    public function getRandomPopularNounsAsString(int $count): string
+    public function getRandomPopularNounsAsString(int $count, string $delimeter = ', '): string
     {
-        return implode(', ', $this->getRandomPopularNouns($count));
+        return implode($delimeter, $this->getRandomPopularNouns($count));
     }
 
     public function getRandomPopularNouns(int $count): array
@@ -68,5 +68,24 @@ class StringHelper
     public function getPopularNounsAsString(): string
     {
         return (new FileHelper())->getFileContent(__DIR__ . '/String/PopularNouns.txt');
+    }
+
+    // убирает отступы у строк слева, чтобы когда мы заменим PHP_EOL на '</p><p>', мы спокойно убрали лишние теги,
+    // например до функции было: '<p>  <li>', а после '<p>  <li>', которые мы можем заменить на <li>
+    public function getHtmlWithoutIndentions(string $html)
+    {
+        // делаем единообразные переносы строк (вдруг попадется текст написанный на Windows):
+        $html = str_replace("\r\n", PHP_EOL, $html);
+
+        // удаляем пробелы между реальными строками:
+        $paragraphs = explode(PHP_EOL, $html);
+        $paragraphs = array_map(fn($paragraph) => trim($paragraph), $paragraphs);
+
+        // удаляем пустые строки:
+        $paragraphs = array_filter($paragraphs);
+        // снова соединяем все строки в один текст:
+        $html = implode(PHP_EOL, $paragraphs);
+        
+        return trim($html);
     }
 }
--- tests/StringHelperTest.php
+++ tests/StringHelperTest.php
@@ -2,6 +2,7 @@
 
 namespace YaPro\Helper\Tests;
 
+use Generator;
 use PHPUnit\Framework\TestCase;
 use YaPro\Helper\StringHelper;
 
@@ -12,5 +13,39 @@ class StringHelperTest extends TestCase
         $object = new StringHelper();
         $actual = $object->transliterate(' Привет Мир $! ');
         $this->assertSame('privet-mir', $actual);
+    }
+
+
+    public function providerGetHtmlWithoutIndentions(): Generator
+    {
+        yield [
+            'html' => '<h2>word word</h2>
+<p>word word</p>
+
+<h2>word word</h2>
+<p>word word.</p>
+<ul>
+  <li>word word.</li>
+</ul>
+<p>word word.</p>',
+            'expected' => '<h2>word word</h2>' . PHP_EOL .
+                '<p>word word</p>' . PHP_EOL .
+                '<h2>word word</h2>' . PHP_EOL .
+                '<p>word word.</p>' . PHP_EOL .
+                '<ul>' . PHP_EOL .
+                '<li>word word.</li>' . PHP_EOL .
+                '</ul>' . PHP_EOL .
+                '<p>word word.</p>',
+        ];
+    }
+
+    /**
+     * @dataProvider providerGetHtmlWithoutIndentions()
+     */
+    public function testGetHtmlWithoutIndentions(string $html, string $expected): void
+    {
+        $object = new StringHelper();
+        $actual = $object->getHtmlWithoutIndentions($html);
+        $this->assertSame($expected, $actual);
     }
 }
