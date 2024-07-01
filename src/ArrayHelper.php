d227985f68c47e486ce87b01de8cb67c3ab9892f
Lebnik <totx@ya.ru>
2024-07-01 16:45:35 +0000

    src/ArrayHelper has been deprecated

---
+++ src/ArrayHelper.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types = 1);
+
+namespace YaPro\Helper;
+
+/**
+ * @deprecated use YaPro\Helper\Array\ArrayHelper
+ */
+class ArrayHelper extends \YaPro\Helper\Array\ArrayHelper
+{
+}
