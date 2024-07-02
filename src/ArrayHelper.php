0240f6fe635ac33927217545f150e5a09915325f
Lebnik <totx@ya.ru>
2024-07-02 06:51:17 +0000

    Renaming the Array to the Arrays

--- src/ArrayHelper.php
+++ src/ArrayHelper.php
@@ -6,6 +6,6 @@ namespace YaPro\Helper;
 /**
  * @deprecated use YaPro\Helper\Array\ArrayHelper
  */
-class ArrayHelper extends \YaPro\Helper\Array\ArrayHelper
+class ArrayHelper extends \YaPro\Helper\Arrays\ArrayHelper
 {
 }
